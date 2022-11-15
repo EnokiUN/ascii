a8f62ca0c3cdcaa4e1b7e5a98e15d86762c11291
Enoki <enokiun@gmail.com>
2022-11-15 19:21:14 +0300

    feat: change to step_by()

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,17 @@ fn main() {
         .unwrap();
     let img = Reader::open(filename).unwrap().decode().unwrap().to_rgba8();
 
-    img.rows().enumerate().for_each(|(i, p)| {
-        if i % (scale * 2) == 0 {
-            p.enumerate().for_each(|(i, p)| {
-                if i % scale == 0 {
-                    let rgba = p.0;
-                    if rgba[3] == 0 {
-                        print!(" ");
-                    } else {
-                        let opacity =
-                            rgba[0] as u16 + rgba[1] as u16 + rgba[2] as u16 + rgba[3] as u16;
-                        let value = opacity / divisor as u16;
-                        print!("{}", symbols[value as usize]);
-                    }
-                }
-            });
-            println!("");
-        }
+    img.rows().step_by(scale * 2).for_each(|p| {
+        p.step_by(scale).for_each(|p| {
+            let rgba = p.0;
+            if rgba[3] == 0 {
+                print!(" ");
+            } else {
+                let opacity = rgba[0] as u16 + rgba[1] as u16 + rgba[2] as u16 + rgba[3] as u16;
+                let value = opacity / divisor as u16;
+                print!("{}", symbols[value as usize]);
+            }
+        });
+        println!("");
     });
 }
